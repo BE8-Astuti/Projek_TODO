2ff72a3ebfe64bac6932ebbdaeba45aee2cf6b17
Astuti <100201094+astutirahmawati@users.noreply.github.com>
2022-05-21 08:59:55 +0700

    update

--- delivery/view/Projek/request.go
+++ delivery/view/Projek/request.go
@@ -1,14 +1,16 @@
 package projek
 
+
 type InsertPro struct {
-	UserID   uint   `json:"userid" validate:"required"`
-	Title string   `json:"title" validate:"required"`
-	Description     string `json:"description" validate:"required"`
+	UserID      uint   `json:"userid" validate:"required"`
+	Title       string `json:"title" validate:"required"`
+	Description string `json:"description" validate:"required"`
 	Contributor string `json:"contributor" validate:"required"`
 }
 
+
 type UpdatePro struct {
-	Title string   `json:"title" validate:"required"`
-	Description     string `json:"description" validate:"required"`
+	Title       string `json:"title" validate:"required"`
+	Description string `json:"description" validate:"required"`
 	Contributor string `json:"contributor" validate:"required"`
 }
